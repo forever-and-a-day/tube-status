565679fabc5e9cb8512a52d851b981c3db99ad2f
C-D-Lewis <Bonsitm@gmail.com>
2017-04-29 17:29:23 +0100

    Update log, add evt-daily.js

--- backend/src/modules/server.js
+++ backend/src/modules/server.js
@@ -1,7 +1,8 @@
 var express = require('express');
 
-var ledServerClient = require('../common/led-server-client.js');
 var config = require('../common/config.js');
+var evtDaily = require('../common/evt-daily.js');
+var ledServerClient = require('../common/led-server-client.js');
 var log = require('../common/log.js');
 
 var app = express();
@@ -10,6 +11,8 @@ function setup() {
   app.get('/status', function(req, res) {
     log.verbose('Status requested.');
     ledServerClient.blink(6, [0, 0, 20]);
+    evtDaily.increment();
+
     res.write('OK\n');
     res.end();
   });
